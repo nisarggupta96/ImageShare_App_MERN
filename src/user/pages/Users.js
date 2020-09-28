0f8a147aca69be088c72b90fc1208ad7232202c4
Nisarg <nisarg.gupta@moovweb.com>
2020-09-28 23:05:05 +0530

    Navigation, Main Header, Portal, Side Drawer

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,8 +5,8 @@ import UsersList from '../components/UsersList';
 const Users = () => {
 
   const USERS = [
-    { id: 'u1', image: 'https://dummyimage.com/400', name: 'name1', places: 1 },
-    { id: 'u2', image: 'https://dummyimage.com/400', name: 'name2', places: 2 },
+    { id: 'u1', image: 'https://dummyimage.com/200', name: 'name1', places: 1 },
+    { id: 'u2', image: 'https://dummyimage.com/200', name: 'name2', places: 2 },
   ];
 
   return <UsersList items={USERS} />;
