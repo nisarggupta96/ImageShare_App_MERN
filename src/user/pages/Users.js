68def35e8cb4a1e72330b2397fdb0de39119d922
Nisarg <nisarg.gupta@moovweb.com>
2020-09-28 21:44:25 +0530

    UserList, UserItem, Cards Component Init

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Dummy Text</h2>;
+
+  const USERS = [
+    { id: 'u1', image: 'https://dummyimage.com/400', name: 'name1', places: 1 },
+    { id: 'u2', image: 'https://dummyimage.com/400', name: 'name2', places: 2 },
+  ];
+
+  return <UsersList items={USERS} />;
 }
 
 export default Users;
